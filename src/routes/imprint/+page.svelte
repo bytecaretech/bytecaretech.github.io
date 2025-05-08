9b38ad2b3faac02139140b11c1c8ad7a21d862f7
Stefan Langenbach <stefan.langenbach@bytecare.tech>
2025-05-08 18:36:17 +0200

    feat(legal): add imprint and privacy policy

--- src/routes/imprint/+page.svelte
+++ src/routes/imprint/+page.svelte
@@ -1,4 +1,41 @@
 <section>
-	<h1 class="px-4 py-4 text-4xl font-bold">Imprint</h1>
-	<p class="px-4 py-4">This page is under construction.</p>
+	<div class="mx-auto max-w-4xl px-4 py-12">
+		<h1 class="mb-4 text-3xl font-bold">Impressum</h1>
+		<p class="mb-4">
+			byteCare UG (haftungsbeschränkt)<br />
+			Am Hangeweiher 3<br />
+			52074 Aachen
+		</p>
+		<p class="mb-4">
+			Handelsregister: HRB 27913<br />
+			Registergericht: Amtsgericht Aachen
+		</p>
+
+		<p class="mb-4">
+			<strong>Vertreten durch:</strong><br />
+			Stefan Langenbach
+		</p>
+
+		<h2 class="text-lg font-semibold">Kontakt</h2>
+		<p class="mb-4">
+			Telefon: +49-241-95704253<br />
+			E-Mail: hello@bytecare.tech
+		</p>
+
+		<h2 class="text-lg font-semibold">Umsatzsteuer-ID</h2>
+		<p class="mb-4">
+			Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />
+			DE369538465
+		</p>
+
+		<h2 class="text-lg font-semibold">
+			Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+		</h2>
+		<p class="mb-4">
+			Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+			Verbraucherschlichtungsstelle teilzunehmen.
+		</p>
+
+		<p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
+	</div>
 </section>
