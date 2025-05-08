83350f1a119fb48e604736cfcc3520f8cb7ab13f
Stefan Langenbach <stefan.langenbach@bytecare.tech>
2025-05-08 18:34:06 +0200

    style(colors): use brand fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: "#6755D3",
+				secondary: "#9386EE",
+				accent: "#F5F5F5",
+				"brand-black": "#333333",
+				"brand-blue": "#20204C"
+			}
+		}
 	},
 	plugins: []
 };
